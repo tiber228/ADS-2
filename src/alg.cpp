29e64ea179aa77a966d13fa91e8c03cb4e0f408e
tiber228 <79192100+tiber228@users.noreply.github.com>
2021-04-09 02:32:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,2 @@
 // Copyright 2021 NNTU-CS
 
-int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
